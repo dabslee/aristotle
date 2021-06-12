af838508c5514566a3483a5177d9b4350e4fe3c3
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-06-12 11:42:26 -0500

    added courses model

--- forum/models.py
+++ forum/models.py
@@ -2,3 +2,14 @@ from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='owner'
+    )
+    students = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='student'
+    )
