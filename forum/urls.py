91c14c04675171d4bae4147c4772f79e8fa9f14a
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-06-12 12:43:34 -0500

    added course selection

--- forum/urls.py
+++ forum/urls.py
@@ -6,4 +6,5 @@ app_name = 'forum'
 urlpatterns = [
     path('', views.index, name='index'),
     path('courses/', views.courses, name='courses'),
+    path('setcourse/<course_id>', views.setcourse, name='setcourse')
 ]
--- forum/views.py
+++ forum/views.py
@@ -3,16 +3,30 @@ from django.contrib.auth.models import Permission, User
 from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist
 
+from .models import Course
+
+def alwaysContext(request):
+    return {
+        "username": request.user.username,
+        "selected_course": Course.objects.filter(id=request.session.get('selected_course_id')).first()
+    }
+
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return redirect('home')
-    if request.user.is_authenticated:
-        return render(request, "index.html", {})
-    else:
-        return redirect('home')
+    context = alwaysContext(request)
+    return render(request, "index.html", context)
 
 def courses(request):
     if not request.user.is_authenticated:
         return redirect('home')
-    return redirect('home')
+    context = alwaysContext(request)
+    context["courses"] = Course.objects.filter(owner=request.user)
+    return render(request, "courses.html", context)
+
+def setcourse(request, course_id):
+    if not request.user.is_authenticated:
+        return redirect('home')
+    request.session['selected_course_id'] = course_id
+    return redirect("forum:courses")
