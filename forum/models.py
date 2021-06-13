2a60d6ebc976722a1748e5c659ec1596f85a28f7
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-06-13 09:11:54 -0500

    added assignment and submission models

--- forum/models.py
+++ forum/models.py
@@ -13,3 +13,24 @@ class Course(models.Model):
         settings.AUTH_USER_MODEL,
         related_name='course_of_student'
     )
+
+class Assignment(models.Model):
+    course = models.ForeignKey(Course)
+    title = models.CharField(max_length=100)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    description = models.CharField(max_length=10000)
+    total_points = models.IntegerField(null=True, blank=True)
+
+class Submission(models.Model):
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="submission_of_student"
+    )
+    assignment = models.ForeignKey(
+        Assignment,
+        on_delete=models.CASCADE,
+        related_name="submission_of_assignment"
+    )
+    earned_points = models.IntegerField(null=True, blank=True)
