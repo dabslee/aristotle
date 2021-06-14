1bc5cc4576199664fd4f73b54b72993698f9c197
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-06-13 22:07:57 -0500

    added aristotle subdomain to allowed hosts

--- aristotle/settings.py
+++ aristotle/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'aristotle.brandonssandbox.com']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
