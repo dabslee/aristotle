480295d0e6085ef73635c8a224e32f970370a389
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-08-25 11:08:04 -0500

    fixed some more assignment.name bugs

--- forum/models.py
+++ forum/models.py
@@ -80,7 +80,7 @@ class Grade(models.Model):
     feedback = models.CharField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
-        return self.assignment.name + " [grade]"
+        return self.assignment.title + " [grade]"
 
 class Submission(models.Model):
     student = models.ForeignKey(
