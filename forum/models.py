0c2619515576451d26dd61eae1d1df1899d2a7e5
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-08-26 16:16:22 -0500

    reverted co-teacher changes

--- forum/models.py
+++ forum/models.py
@@ -17,11 +17,6 @@ class Course(models.Model):
         related_name='course_of_student',
         blank=True
     )
-    coteachers = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        related_name='course_of_coteacher',
-        blank=True
-    )
     uuid = models.UUIDField(
         default=uuid.uuid4,
         editable=False
