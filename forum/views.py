675861a2e657eedcc87de7d545f5308cca6ce13d
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-09-06 12:12:32 -0500

    added functionality to upcoming assn on front page

--- forum/views.py
+++
@@ -1,9 +0,0 @@
-from forum.views_folder.utilities import alwaysContext
-from django.shortcuts import render, redirect
-
-# Create your views here.
-def index(request):
-    if not request.user.is_authenticated:
-        return redirect('home')
-    context = alwaysContext(request)
-    return render(request, "index.html", context)
--- forum/views_folder/views.py
+++ forum/views_folder/views.py
@@ -1,9 +1,20 @@
+from forum.views_folder.utilities import alwaysContext
 from django.shortcuts import render, redirect
-from .utilities import alwaysContext
+from ..models import Assignment
+from django.db.models import Q
+import datetime
 
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return redirect('home')
     context = alwaysContext(request)
+    context["assignments"] = Assignment.objects.filter(
+        Q(course__owner=request.user)
+        | Q(course__students=request.user)
+        & Q(end_datetime__gte=str(datetime.datetime.now()))
+        & Q(end_datetime__lte=str(datetime.datetime.now() + datetime.timedelta(days=7)))
+    ).exclude(
+        end_datetime__isnull=True
+    )
     return render(request, "index.html", context)
